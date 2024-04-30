068afe81c527b314471da385d1af19202e1232b2
mauricioestradadlg <mauricioestrada580@gmail.com>
2024-04-30 11:56:36 -0600

    Algoritmo de prueba

--- evidencia3.java
+++ evidencia3.java
@@ -1,8 +1,42 @@
 
+import java.util.Scanner;
+
+
 public class evidencia3 {
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+
+
+        //INICIO
         
+        System.out.print("Ingrese el tiempo de ejecución en segundos: ");
+        int duration = scanner.nextInt();
         
+        long start = System.currentTimeMillis();
+        long end = start + duration * 1000; // Convertir segundos a milisegundos
+
+
+
+        //ALGORITMO
+        
+        while (System.currentTimeMillis() < end) {
+            System.out.println("Hola mundo");
+            // Pausa de 1 segundo para no imprimir demasiado rápido
+            try {
+                Thread.sleep(1000); // Pausa de 1 segundo
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+
+        //FIN
+        
+        System.out.println("Tiempo de ejecución terminado.");
+        scanner.close();
     }
+
 }
+
+
